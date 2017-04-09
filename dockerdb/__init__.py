eaff5ce5dc13b0f2b4f44760a2fcb6c30f9c9687
Florian Ludwig <f.ludwig@greyrook.com>
2017-04-09 20:16:06 +0200

    imrpoved testing and some cleanup

--- dockerdb/__init__.py
+++ dockerdb/__init__.py
@@ -2,18 +2,25 @@ import time
 
 import docker
 
-
+start_time = int(time.time())
+counter = 0
 client = docker.from_env()
 
 
 class Service(object):
     """Base class for docker based services"""
     timeout = 30.0
+    name = 'service'
 
     def __init__(self, image, **kwargs):
+        global counter
+
         self.client = client
         kwargs.setdefault('detach', True)
+        name = 'tmp_{}_{}_{}'.format(start_time, self.name, counter)
+        kwargs.setdefault('name', name)
         self.container = client.containers.run(image, **kwargs)
+        counter += 1
 
     def inspect(self):
         """get docker inspect data for container"""
@@ -40,7 +47,27 @@ class Service(object):
             pass
 
 
+class HTTPServer(Service):
+    protocol = 'http'
+    port = '80'
+
+    def check_ready(self):
+        import requests
+
+        ip = self.ip_address()
+        url = '{}://{}:{}'.format(self.protocol, ip, self.port)
+
+        try:
+            requests.get(url)
+            return True
+        except requests.exceptions.ConnectionError:
+            return False
+
+
 class Mongo(Service):
+    name = 'mongo'
+    port = 27017
+
     def __init__(self, tag, wait=False, **kwargs):
         Service.__init__(self, 'mongo:' + tag)
         if wait:
@@ -62,5 +89,4 @@ class Mongo(Service):
         import pymongo.errors
 
         server = self.ip_address()
-        port = 27017
-        return pymongo.MongoClient(server, port, socketTimeoutMS=1000, connectTimeoutMS=1000)
+        return pymongo.MongoClient(server, self.port, socketTimeoutMS=100, connectTimeoutMS=100)
--- tests/test_dockerdb.py
+++ tests/test_dockerdb.py
@@ -4,17 +4,54 @@
 import dockerdb
 
 
-def test_helloworld():
+class LazyHTTP(dockerdb.HTTPServer):
+    port = 8000
+
+    def __init__(self, wait=False, **kwargs):
+        command = [
+            'python3',
+            '-c',
+            'import http.server; import time; time.sleep(0.1); http.server.test(port=8000)'
+        ]
+        dockerdb.Service.__init__(self, 'python:3', command=command)
+
+
+def get_all_container_ids():
+    all_containers = dockerdb.client.containers.list()
+    return [c.id for c in all_containers]
+
+
+def test_remove():
+    mongo = dockerdb.Mongo(tag='3.5', wait=True)
+
+    assert mongo.container.id in get_all_container_ids()
+    mongo.remove()
+    assert mongo.container.id not in get_all_container_ids()
+
+
+def test_http_and_wait():
+    http_server = LazyHTTP(wait=False)
+    assert http_server.check_ready() == False
+    http_server.wait()
+    assert http_server.check_ready() == True
+
+
+def test_timeout():
     pass
 
 
-def test_docker():
+def test_mongo():
     mongo = dockerdb.Mongo(tag='3.5', wait=True)
+
+    start_time = str(dockerdb.start_time)
+    assert start_time in mongo.container.name
+    assert 'mongo' in mongo.container.name
+
     client = mongo.pymongo_client()
     server_info = client.server_info()
     assert server_info['version'].startswith('3.5')
-
-    mongo = dockerdb.Mongo(tag='3.4', wait=True)
-    client = mongo.pymongo_client()
-    server_info = client.server_info()
-    assert server_info['version'].startswith('3.4')
+    #
+    # mongo = dockerdb.Mongo(tag='3.4', wait=True)
+    # client = mongo.pymongo_client()
+    # server_info = client.server_info()
+    # assert server_info['version'].startswith('3.4')
